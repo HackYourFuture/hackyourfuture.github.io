b264808cc9b9f0a43963a3c75fd29ebbb8294673
fadi rasheed <fadi rasheed>
2018-08-10 13:24:09 +0200

    call findApplicant and check if the deadline is not passed

--- api/src/middlewares/upload.js
+++ api/src/middlewares/upload.js
@@ -1,11 +1,14 @@
 const {
-    updateApplicant
+    updateApplicant,
+    findApplicant // to be made later
 } = require("../data");
 
 const sendEmail = require("../sendEmail");
 
 const fromEmail = "info@hackyourfuture.net";
 
+const deadline = new Date(); // to be filled later with the deadline
+const now = new Date();
 
 module.exports = (req, res) => {
     const {
@@ -13,21 +16,33 @@ module.exports = (req, res) => {
         assignmentLink
     } = req.body;
 
-    updateApplicant(email, assignmentLink, req.files)
-        .then(() => {
-            return sendEmail(
-                fromEmail,
-                [email],
-                '** Confirmation email **',
-                'We\'ve received your files'
-            );
-        }).then(() => {
-            res.send({
-                message: 'you got an email :-)'
-            });
-        }).catch(() => {
-            res.status(500).send({
-                message: 'Something went wrong'
-            });
-        })
+    if(now <= deadline) {
+        findApplicant(email)
+            .then(()=>
+                updateApplicant(email, assignmentLink, req.files)
+                    .then(() => {
+                        return sendEmail(
+                        fromEmail,
+                        [email],
+                        '** Confirmation email **',
+                        'We\'ve received your files'
+                        );
+                    })
+                    .then(() => {
+                        res.send({
+                        message: 'you got an email :-)'
+                    });
+                    })
+                    .catch(() => {
+                        res.status(500).send({
+                        message: 'Something went wrong'
+                        });
+                    })
+            )
+            .catch(() => {
+                res.send("Your name does not exist")
+            })
+    } else {
+            res.send("the deadline has passed")
+        }
 };
