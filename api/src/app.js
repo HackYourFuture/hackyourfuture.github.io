e33c5d9c0907251491056f2b59a983ce037f4a93
hadeel alabrash <hadeelalabrash@hadeels-air.home>
2018-08-14 18:26:32 +0200

    Validate file names / mime-type in upload endpoint with wael

--- api/src/app.js
+++ api/src/app.js
@@ -13,7 +13,25 @@ const {
 const app = express();
 
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+
+const upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        fileType(file, cb);
+    }
+
+});
+
+function fileType(file, cb) {
+    const fileTypes = ['.jpg', '.jpeg', '.bmp', '.gif', '.png', '.txt', '.doc', '.docs'];
+    const mimeType = fileTypes.test(file.mimeType);
+    if (mimeType) {
+        return cb(null, true);
+    }
+    cb("Error: File upload only supports the following filetypes :" + fileTypes);
+}
+
+
 const FileUpload = upload.fields(
     [{
         name: 'files',
@@ -23,30 +41,65 @@ const FileUpload = upload.fields(
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(expressValidator());
 
 app.post('/apply', (req, res) => {
-    req.check("userName", "Username is too short").isLength({min: 3}).isString();
-    req.check("street", "Street name is too short").isLength({min: 3}).isString();
-    req.check("city", "City name is too short or too long").isLength({min: 3}).isString();
+    req.check("userName", "Username is too short").isLength({
+        min: 3
+    }).isString();
+    req.check("street", "Street name is too short").isLength({
+        min: 3
+    }).isString();
+    req.check("city", "City name is too short or too long").isLength({
+        min: 3
+    }).isString();
     req.check("email", "Invalid Email Address").isEmail();
-    req.check("phone", "Invalid phone number").isNumeric().isLength({min: 9});
-    req.check("education", "The string must be between 2-15 letters").isLength({min: 3}).isString()
-    req.check("how_hear", "Either not a String or the String is too long").isString().isLength({max: 20});
+    req.check("phone", "Invalid phone number").isNumeric().isLength({
+        min: 9
+    });
+    req.check("education", "The string must be between 2-15 letters").isLength({
+        min: 3
+    }).isString()
+    req.check("how_hear", "Either not a String or the String is too long").isString().isLength({
+        max: 20
+    });
     req.check("computer", "Invalid boolean").isBoolean();
 
     let errors = req.validationErrors();
 
-    if(errors){
+    if (errors) {
         console.error("Validation errors: ", errors);
-        res.status(500).json({ errors });
-    } else{
+        res.status(500).json({
+            errors
+        });
+    } else {
         Apply(req, res)
     }
 });
+const allowedFileTypes = ['jpeg', 'jpg', 'png', 'gif'];
+const fieldName = ['input_file_cv', 'input_file_motivation_letter '];
 app.post('/contact-us', (req, res) => ContactUs(req, res));
 app.post('/apply', (req, res) => Apply(req, res));
-app.post('/upload', FileUpload, (req, res) => Upload(req, res));
+app.post('/upload', FileUpload, (req, res) => {
+
+    fieldName.forEach((element) => {
+        if (file.fieldname === element) {
+            fileType(file, cb)
+            Upload(req, res)
+        }
+    });
+    if (file.fieldname === 'myImage') {
+        allowedFileTypes.forEach((element) => {
+            if (file.mimetype !== element) {
+                return cb(null, false, new Error("Error: File upload only supports the following filetypes :" + allowedFileTypes));
+            }
+            cb(null, true);
+            Upload(req, res)
+        });
+    }
+});
 
 module.exports = app;
