4f765c041e2c0fbb41635b5594662d48df215502
hadeel alabrash <hadeelalabrash@hadeels-air.home>
2018-08-17 13:16:58 +0200

    update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const expressValidator = require("express-validator");
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const multer = require('multer');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const multer = require("multer");
 
 const {
     Apply,
     ContactUs,
-    Upload,
-} = require('./middlewares');
+    Upload
+} = require("./middlewares");
 
 const app = express();
 
@@ -19,74 +19,103 @@ const upload = multer({
     fileFilter: (req, file, cb) => {
         fileType(file, cb);
     }
-
 });
-
-
-const fileTypes = [".jpg", ".jpeg", ".bmp", ".gif", ".png", ".txt", ".doc", ".docs"];
-const allowedMimeTypes = ["text/plain", "application/msword", "application/x-pdf", "application/pdf",
-    "image/gif", "image/jpeg", "image/png", "text/plain"
+const fileTypes = [
+    ".jpg",
+    ".jpeg",
+    ".bmp",
+    ".gif",
+    ".png",
+    ".txt",
+    ".doc",
+    ".docs"
 ];
-const fields = ["input_file_cv", "input_file_motivation_letter", "input_file_assignment"];
+const allowedMimeTypes = [
+    "text/plain",
+    "application/msword",
+    "application/x-pdf",
+    "application/pdf",
+    "image/gif",
+    "image/jpeg",
+    "image/png",
+    "text/plain"
+];
+const fields = [
+    "input_file_cv",
+    "input_file_motivation_letter",
+    "input_file_assignment"
+];
 
 function fileType(file, cb) {
-
     const fileTypeAccepted = allowedMimeTypes.includes(file.mimetype);
     const fieldNameAccepted = fields.includes(file.fieldname);
     if (fieldNameAccepted) {
         if (fileTypeAccepted) {
-            return cb([file.mimetype, file.fieldname]);
+            return cb(null, true);
         }
+
         cb(
-            "Error: File upload only supports the following filetypes :" + fileTypes
+            new Error(
+                "File upload only supports the following filetypes :" +
+                fileTypes
+            )
         );
     }
-    return cb("Error: Something went Wrong ");
+    cb(new Error("Something went wrong."));
 }
 
-
-
-const FileUpload = upload.fields(
-    [{
-        name: 'input_file_cv',
+const FileUpload = upload.fields({
+    name: "input_file_cv",
+    maxCount: 1
+}, {
+        name: "input_file_motivation_letter",
         maxCount: 1
     }, {
-        name: 'input_file_motivation_letter',
+        name: "input_ file_assignment",
         maxCount: 1
-    }, {
-        name: 'input_ file_assignment',
-        maxCount: 1
-    }]
-);
-
+    });
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
 app.use(expressValidator());
 
-app.post('/apply', (req, res) => {
-    req.check("userName", "Username is too short").isLength({
-        min: 3
-    }).isString();
-    req.check("street", "Street name is too short").isLength({
-        min: 3
-    }).isString();
-    req.check("city", "City name is too short or too long").isLength({
-        min: 3
-    }).isString();
+app.post("/apply", (req, res) => {
+    req.check("userName", "Username is too short")
+        .isLength({
+            min: 3
+        })
+        .isString();
+    req.check("street", "Street name is too short")
+        .isLength({
+            min: 3
+        })
+        .isString();
+    req.check("city", "City name is too short or too long")
+        .isLength({
+            min: 3
+        })
+        .isString();
     req.check("email", "Invalid Email Address").isEmail();
-    req.check("phone", "Invalid phone number").isNumeric().isLength({
-        min: 9
-    });
-    req.check("education", "The string must be between 2-15 letters").isLength({
-        min: 3
-    }).isString()
-    req.check("how_hear", "Either not a String or the String is too long").isString().isLength({
-        max: 20
-    });
+    req.check("phone", "Invalid phone number")
+        .isNumeric()
+        .isLength({
+            min: 9
+        });
+    req.check("education", "The string must be between 2-15 letters")
+        .isLength({
+            min: 3
+        })
+        .isString();
+    req.check("how_hear", "Either not a String or the String is too long")
+        .isString()
+        .isLength({
+            max: 20
+        });
     req.check("computer", "Invalid boolean").isBoolean();
 
     let errors = req.validationErrors();
@@ -97,14 +126,11 @@ app.post('/apply', (req, res) => {
             errors
         });
     } else {
-        Apply(req, res)
+        Apply(req, res);
     }
 });
 
-app.post('/contact-us', (req, res) => ContactUs(req, res));
-app.post('/apply', (req, res) => Apply(req, res));
-app.post('/upload', FileUpload, (req, res) => {
-    Upload(req, res);
-    fileType(file, cb);
-});
+app.post("/contact-us", (req, res) => ContactUs(req, res));
+app.post("/apply", (req, res) => Apply(req, res));
+app.post("/upload", FileUpload, (req, res) => Upload(req, res));
 module.exports = app;
