2aa80f8563c68e2f6e61afa1720fa99180b478f7
hadeel alabrash <hadeelalabrash@hadeels-MacBook-Air.local>
2018-08-26 15:50:54 +0200

    Split upload endpoints

---
+++ api/src/middlewares/upload_assignment.js
@@ -0,0 +1,49 @@
+const { updateApplicant } = require("../data");
+
+const { getApplicant } = require("../data/update-sheet");
+
+const sendEmail = require("../utils/send-emails");
+
+const fromEmail = "info@hackyourfuture.net";
+
+const deadline = new Date(); // to be filled later with the deadline
+const now = new Date();
+
+module.exports = (req, res) => {
+    const { email, url, message } = req.body;
+    const assignmentUrl = req.files.input_file_assignment[0].location;
+    const updatedUrlAssignment = {
+        assignmentUrl,
+        message,
+        url
+    };
+    if (now <= deadline) {
+        getApplicant(email)
+            .then(() =>
+                updateApplicant(email, updatedUrlAssignment, req.files)
+                    .then(() => {
+                        return sendEmail(
+                            fromEmail,
+                            [email],
+                            "** Confirmation email **",
+                            "We've received your files"
+                        );
+                    })
+                    .then(() => {
+                        res.send({
+                            message: "you got an email :-)"
+                        });
+                    })
+                    .catch(() => {
+                        res.status(500).send({
+                            message: "Something went wrong"
+                        });
+                    })
+            )
+            .catch(() => {
+                res.status(404).send("Your name does not exist");
+            });
+    } else {
+        res.status(400).send("the deadline has passed");
+    }
+};
