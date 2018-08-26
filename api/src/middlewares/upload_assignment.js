83b0c4c3c9bdf62cacfdd66f68bb347090bf9058
MURATKAYMAZ56 <muratkaymaz56@gmail.com>
2018-08-26 22:19:43 +0200

    triggered email in  assignmend and cv file to the applicant and admin

--- api/src/middlewares/upload_assignment.js
+++ api/src/middlewares/upload_assignment.js
@@ -1,6 +1,10 @@
-const { updateApplicant } = require("../data");
+const {
+    updateApplicant
+} = require("../data");
 
-const { getApplicant } = require("../data/update-sheet");
+const {
+    getApplicant
+} = require("../data/update-sheet");
 
 const sendEmail = require("../utils/send-emails");
 
@@ -10,7 +14,11 @@ const deadline = new Date(); // to be filled later with the deadline
 const now = new Date();
 
 module.exports = (req, res) => {
-    const { email, url, message } = req.body;
+    const {
+        email,
+        url,
+        message
+    } = req.body;
     const assignmentUrl = req.files.input_file_assignment[0].location;
     const updatedUrlAssignment = {
         assignmentUrl,
@@ -21,36 +29,35 @@ module.exports = (req, res) => {
         getApplicant(email)
             .then(() =>
                 updateApplicant(email, updatedUrlAssignment, req.files)
-                    .then(() => {
-                        sendEmail(
-                            fromEmail,
-                            [email],
-                            "** Confirmation email **",
-                            "We've received your files"
-                        );
-                        sendEmail(
-                            fromEmail,
-                            applicationMail,
-                            "** Confirmation email **",
-                            "Applicant upload successfully"
-                        );
-                    })
-                    .then(() => {
-                        res.send({
-                            message: "you got an email :-)"
-                        });
-                    })
-                    .catch(() => {
-                        sendEmail(
-                            fromEmail,
-                            fromEmail,
-                            "** Confirmation email **",
-                            `Upload file is failed:${[email]}`
-                        );
-                        res.status(500).send({
-                            message: "Something went wrong"
-                        });
-                    })
+                .then(() => {
+                    sendEmail(
+                        fromEmail, [email],
+                        "** Confirmation email **",
+                        "We've received your files"
+                    );
+                    sendEmail(
+                        fromEmail,
+                        applicationMail,
+                        "** Confirmation email **",
+                        "Applicant uploaded Assignment successfully"
+                    );
+                })
+                .then(() => {
+                    res.send({
+                        message: "you got an email :-)"
+                    });
+                })
+                .catch(() => {
+                    sendEmail(
+                        fromEmail,
+                        fromEmail,
+                        "** Confirmation email **",
+                        `Uploading Assignment file is failed:${[email]}`
+                    );
+                    res.status(500).send({
+                        message: "Something went wrong"
+                    });
+                })
             )
             .catch(() => {
                 res.status(404).send("Your name does not exist");
