a7d7065b2aa93d920bc0fbd6ed54690c05f7c9bd
SarKurd <sarbast2009@hotmail.co.uk>
2018-08-27 16:36:52 +0200

    implemented update-applicant

--- api/src/data/update-applicant.js
+++ api/src/data/update-applicant.js
@@ -4,8 +4,27 @@
  *
  * @param {string} email email address of an existing applicant
  * @param {object} updates object containing the applicant's new or updated fields
- * @param {object[]} files files to be stored to S3
  */
-async function updateApplicant() {}
+
+const { getApplicant, saveApplicant } = require("./update-sheet");
+
+async function updateApplicant(email, updates) {
+    let foundedAt;
+    try {
+        const res = await getApplicant(email);
+        foundedAt = res.foundedAt;
+        if (!foundedAt) {
+            throw new Error("Applicant doesn't exist");
+        }
+    } catch (error) {
+        if (error.message === "Applicant doesn't exist") {
+            return error;
+        }
+    }
+
+    saveApplicant(foundedAt, updates).catch(error => {
+        console.log("Update Applicant FAILED:", error, email);
+    });
+}
 
 module.exports = updateApplicant;
