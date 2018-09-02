ea19b66ac0b717a60ef94b3e15172eee507ce2c0
Sob7i <sub7i.alkhuder@gmail.com>
2018-09-02 13:17:15 +0200

    The spreadSheet-scriptEditor which sends an accept-reject email automatically

--- api/src/data/update-sheet.js
+++ api/src/data/update-sheet.js
@@ -3,9 +3,9 @@ const { google } = require("googleapis");
 const { getClient } = require("../utils/google-auth");
 
 const {
-    getConfig,
-    updateTokenConfig,
-    saveConfig
+  getConfig,
+  updateTokenConfig,
+  saveConfig
 } = require("../utils/dev-config");
 
 const config = getConfig();
@@ -15,128 +15,125 @@ const SHEET_ID = config.spreadSheetId;
 const auth = getClient();
 
 const columnPosition = {
-    userName: 0,
-    street: 1,
-    city: 2,
-    email: 3,
-    phone: 4,
-    education: 5,
-    how_hear: 6,
-    computer: 7
+  userName: 0,
+  street: 1,
+  city: 2,
+  email: 3,
+  phone: 4,
+  education: 5,
+  how_hear: 6,
+  computer: 7,
+  cv: 8,
+  motivation_letter: 9,
+  message_applicant: 10,
+  technical_assignment: 11,
+  screen_shot: 12,
+  message_applicant2: 13
 };
 
 function handleApiError(error) {
-    const { message } = error;
-    const hasTokenError =
-        message.indexOf("invalid") != -1 || message.indexOf("expired") != -1;
+  const { message } = error;
+  const hasTokenError =
+    message.indexOf("invalid") != -1 || message.indexOf("expired") != -1;
 
-    if (!hasTokenError || !process.env.DEVELOPMENT) {
-        console.log("The API returned an error: " + error);
-        return;
-    }
+  if (!hasTokenError || !process.env.DEVELOPMENT) {
+    console.log("The API returned an error: " + error);
+    return;
+  }
 
-    updateTokenConfig(auth).then(token => {
-        config.token = token;
-        saveConfig(config);
-    });
+  updateTokenConfig(auth).then(token => {
+    config.token = token;
+    saveConfig(config);
+  });
 }
 
 function saveApplicant(
-    row,
-    { userName, street, city, email, phone, education, how_hear, computer }
+  row,
+  { userName, street, city, email, phone, education, how_hear, computer }
 ) {
-    return new Promise((resolve, reject) => {
-        if (!row) {
-            reject(new Error("We couldnt save the record"));
-            return;
+  return new Promise((resolve, reject) => {
+    if (!row) {
+      reject(new Error("We couldnt save the record"));
+      return;
+    }
+
+    const sheets = google.sheets("v4");
+
+    const values = [
+      [userName, street, city, email, phone, education, how_hear, computer]
+    ];
+
+    const resource = {
+      valueInputOption: "RAW",
+      data: [
+        {
+          range: `Sheet1!A${row}:H`,
+          majorDimension: "ROWS",
+          values
+        }
+      ]
+    };
+    sheets.spreadsheets.values.batchUpdate(
+      {
+        auth,
+        spreadsheetId: SHEET_ID,
+        valueInputOption: "USER_ENTERED",
+        resource
+      },
+      err => {
+        if (err) {
+          handleApiError(err);
+          reject(err);
+          return;
         }
 
-        const sheets = google.sheets("v4");
-
-        const values = [
-            [
-                userName,
-                street,
-                city,
-                email,
-                phone,
-                education,
-                how_hear,
-                computer
-            ]
-        ];
-
-        const resource = {
-            valueInputOption: "RAW",
-            data: [
-                {
-                    range: `Sheet1!A${row}:H`,
-                    majorDimension: "ROWS",
-                    values
-                }
-            ]
-        };
-        sheets.spreadsheets.values.batchUpdate(
-            {
-                auth,
-                spreadsheetId: SHEET_ID,
-                valueInputOption: "USER_ENTERED",
-                resource
-            },
-            err => {
-                if (err) {
-                    handleApiError(err);
-                    reject(err);
-                    return;
-                }
-
-                resolve();
-                console.log("Spreadsheet is updated");
-            }
-        );
-    });
+        resolve();
+        console.log("Spreadsheet is updated");
+      }
+    );
+  });
 }
 
 function getApplicant(email) {
-    return new Promise((resolve, reject) => {
-        const sheets = google.sheets("v4");
-        sheets.spreadsheets.values.get(
-            {
-                auth,
-                spreadsheetId: SHEET_ID,
-                range: "Sheet1"
-            },
-            (err, response) => {
-                if (err) {
-                    handleApiError(err);
-                    reject(err);
-                    return;
-                }
+  return new Promise((resolve, reject) => {
+    const sheets = google.sheets("v4");
+    sheets.spreadsheets.values.get(
+      {
+        auth,
+        spreadsheetId: SHEET_ID,
+        range: "Sheet1"
+      },
+      (err, response) => {
+        if (err) {
+          handleApiError(err);
+          reject(err);
+          return;
+        }
 
-                const rows = response.data.values || [];
-                const totalRows = (rows.length || 0) + 1;
+        const rows = response.data.values || [];
+        const totalRows = (rows.length || 0) + 1;
 
-                let rowIndex = 0;
+        let rowIndex = 0;
 
-                const foundedAt = rows.reduce((state, row) => {
-                    rowIndex++;
+        const foundedAt = rows.reduce((state, row) => {
+          rowIndex++;
 
-                    const rowEmail = row[columnPosition.email] || "";
+          const rowEmail = row[columnPosition.email] || "";
 
-                    if (rowEmail.toLowerCase() === email.toLowerCase()) {
-                        state = rowIndex;
-                    }
+          if (rowEmail.toLowerCase() === email.toLowerCase()) {
+            state = rowIndex;
+          }
 
-                    return state;
-                }, false);
+          return state;
+        }, false);
 
-                resolve({ foundedAt, totalRows });
-            }
-        );
-    });
+        resolve({ foundedAt, totalRows });
+      }
+    );
+  });
 }
 
 module.exports = {
-    getApplicant,
-    saveApplicant
+  getApplicant,
+  saveApplicant
 };
