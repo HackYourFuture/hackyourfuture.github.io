67325871c01056f573969ef371960c8388c6f726
ideabile <info@ideabile.com>
2019-02-16 19:28:01 +0100

    Remove compression

--- api/main.js
+++ api/main.js
@@ -1,32 +1,6 @@
 const awsServerlessExpress = require("aws-serverless-express");
 const app = require("./src/app");
-
-const binaryMimeTypes = [
-    "application/javascript",
-    "application/json",
-    "application/octet-stream",
-    "application/xml",
-    "application/msword",
-    "application/x-pdf",
-    "application/pdf",
-    "font/eot",
-    "font/opentype",
-    "font/otf",
-    "image/gif",
-    "image/jpeg",
-    "image/png",
-    "image/jpeg",
-    "image/png",
-    "image/svg+xml",
-    "text/comma-separated-values",
-    "text/css",
-    "text/html",
-    "text/javascript",
-    "text/plain",
-    "text/text",
-    "text/xml"
-];
-const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes);
+const server = awsServerlessExpress.createServer(app, null);
 
 exports.handler = (event, context) =>
     awsServerlessExpress.proxy(server, event, context);
