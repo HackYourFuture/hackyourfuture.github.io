c631475fde851ff6bcb4e236beb40b1186fdff1f
Noer Paanakker <noer@hackyourfuture.net>
2019-07-22 11:57:21 +0200

    replaced application form with typeform

--- pages/apply/index.vue
+++ pages/apply/index.vue
@@ -4,17 +4,55 @@
       <div class="apply_page__header">
         <h1>
           Join
-          <br>Us!
+          <br >Us!
         </h1>
         <div class="apply_page__header-image">
-          <img src="/gallery/11.jpg" alt="Group of students working in class.">
+          <img src="/gallery/11.jpg" alt="Group of students working in class." >
         </div>
-        <div class="apply_page__header-dates" v-html="dates"/>
+        <div class="apply_page__header-dates" v-html="dates" />
       </div>
-      <div class="apply_page__content" v-html="content"/>
+      <div class="apply_page__content" v-html="content" />
       <div id="apply_form_ams" ref="apply_page__form" class="apply_page__form form">
         <h3>Apply for our next Amsterdam class:</h3>
-        <FormComponent :action="action" :inputs="inputs"/>
+        <div
+          class="typeform-widget"
+          data-url="https://hackyourfuture.typeform.com/to/PyrEhT"
+          data-transparency="100"
+          data-hide-headers="true"
+          data-hide-footer="true"
+          style="width: 100%; height: 500px;"
+        />
+        <script>
+          (function() {
+          var qs,
+          js,
+          q,
+          s,
+          d = document,
+          gi = d.getElementById,
+          ce = d.createElement,
+          gt = d.getElementsByTagName,
+          id = "typef_orm",
+          b = "https://embed.typeform.com/";
+          if (!gi.call(d, id)) {
+          js = ce.call(d, "script");
+          js.id = id;
+          js.src = b + "embed.js";
+          q = gt.call(d, "script")[0];
+          q.parentNode.insertBefore(js, q);
+          }
+          })();
+        </script>
+        <div
+          style="font-family: Sans-Serif;font-size: 12px;color: #999;opacity: 0.5; padding-top: 5px;"
+        >
+          powered by
+          <a
+            href="https://admin.typeform.com/signup?utm_campaign=PyrEhT&utm_source=typeform.com-14253593-Pro&utm_medium=typeform&utm_content=typeform-embedded-poweredbytypeform&utm_term=EN"
+            style="color: #999"
+            target="_blank"
+          >Typeform</a>
+        </div>
       </div>
     </main>
   </div>
